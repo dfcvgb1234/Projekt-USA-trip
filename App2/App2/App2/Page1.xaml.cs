6d99779cd80e21d7414bf9ab3c406407d1d6e8c0
IceBerg <www.favs2@gmail.com>
2017-03-01 13:37:24 +0100

    Ændret hele projektet til App2, så det virker med nyeste version af xamarin

---
+++ App2/App2/App2/Page1.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace App2
+{
+    public partial class Page1 : ContentPage
+    {
+        int likes;
+        int dislikes;
+        public Page1()
+        {
+            InitializeComponent();
+        }
+        void handle_sliding(object sender, ValueChangedEventArgs args)
+        {
+
+        }
+        void handle_like(object sender, EventArgs args)
+        {
+            likes++;
+            DisplayAlert("title","Du kunne godt lide dette forslag",""+likes);
+        }
+        void handle_dislike(object sender, EventArgs args)
+        {
+            dislikes++;
+            DisplayAlert("title", "Du kunne ikke lide dette forslag", "" + dislikes);
+        }
+    }
+}
